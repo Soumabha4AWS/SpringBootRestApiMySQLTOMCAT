d53533e60b993e1bdb42049298e069d227acc85b
Soumabha4Git <sengupta.soumabha@gmail.com>
2022-11-14 18:46:56 +0530

    first commit

---
+++ src/test/java/com/restapi/mysql/crud/example/SpringBootRestApiMySQLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.mysql.crud.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiMySQLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
